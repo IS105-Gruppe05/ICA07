29fa8027b87b10cca45d8acf3857e65b6923fa08
Mats Skjaervik <matses15@student.uia.no>
2017-05-19 17:58:08 +0200

    fikset oppgave2

--- Oppgave2/tcp_client.go
+++ Oppgave2/tcp_client.go
@@ -7,18 +7,16 @@ import "os"
 
 func main() {
 
-// connect to this socket
-conn, _ := net.Dial("tcp", "158.37.63.213:7070")
-	//conn, _ := net.Dial("tcp", "158.37.63.191:8081")
-for {
-// read in input from stdin
-reader := bufio.NewReader(os.Stdin)
-fmt.Print("Text to send: ")
-text, _ := reader.ReadString('\n')
-// send to socket
-fmt.Fprintf(conn, text + "\n")
-// listen for reply
-message, _ := bufio.NewReader(conn).ReadString('\n')
-fmt.Print("Message from server: "+message)
-}
+	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
+	for {
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+
+		fmt.Fprintf(conn, text+"\n")
+
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
 }
