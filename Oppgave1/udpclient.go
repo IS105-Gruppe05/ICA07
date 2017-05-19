5f7dfa13b5282bc92857f9c64024327c4f70d652
Skjaervik <matses15@student.uia.no>
2017-05-19 18:32:53 +0200

    Update udpclient.go

--- Oppgave1/udpclient.go
+++ Oppgave1/udpclient.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 	p := make([]byte, 2048)
-	conn, err := net.Dial("udp", "158.37.63.22:8084")
+	conn, err := net.Dial("udp", "127.0.0.1:8080")
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
 	}
-	fmt.Fprintf(conn, "Møte Fr 5.5 14:45 Flåklypa")
+	fmt.Fprintf(conn, "Møte Fr 5.5 14:45 Flåklypa")
 	_, err = bufio.NewReader(conn).Read(p)
 	if err == nil {
 		fmt.Printf("%s\n", p)
