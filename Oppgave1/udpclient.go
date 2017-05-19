192813769441a1dae98f211dc165a896af87f94e
Mats Skjaervik <matses15@student.uia.no>
2017-05-19 17:52:04 +0200

    Fikset oppgave1

--- Oppgave1/udpclient.go
+++ Oppgave1/udpclient.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"bufio"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"fmt"
-	"io"
 	"net"
 )
 
@@ -17,39 +13,12 @@ func main() {
 		fmt.Printf("Some error %v", err)
 		return
 	}
-
-	key := []byte("AES256Key-32Characters1234567890")
-	plaintext := []byte("testmeld")
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
-	nonce := make([]byte, 12)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-
-	}
-	fmt.Fprintf(conn, "%x\n", nonce)
-	fmt.Printf("%x\n", nonce)
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
-	fmt.Printf("%x\n", ciphertext)
-
-	fmt.Fprintf(conn, "%x\n", ciphertext)
-
+	fmt.Fprintf(conn, "Møte Fr 5.5 14:45 Flåklypa")
 	_, err = bufio.NewReader(conn).Read(p)
 	if err == nil {
 		fmt.Printf("%s\n", p)
 	} else {
 		fmt.Printf("Some error %v\n", err)
 	}
-
+	conn.Close()
 }
--- Oppgave1/udpserver.go
+++ Oppgave1/udpserver.go
@@ -6,7 +6,6 @@ import (
 )
 
 func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
-
 	_, err := conn.WriteToUDP([]byte("From server: Hello I got your mesage "), addr)
 	if err != nil {
 		fmt.Printf("Couldn't send response %v", err)
@@ -26,37 +25,11 @@ func main() {
 	}
 	for {
 		_, remoteaddr, err := ser.ReadFromUDP(p)
-		fmt.Printf("message recieved from %v \n %s ", remoteaddr, p)
-
+		fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
 		if err != nil {
 			fmt.Printf("Some error  %v", err)
 			continue
-
 		}
-		/*	key := []byte("AES256Key-32Characters1234567890")
-			ciphertext, _ := hex.DecodeString("c6805593e5911e3e7f12dd44c4642e647e0f4c17184632c7")
-
-			nonce, _ := hex.DecodeString("7413e381df7cbf94fd01b4dd")
-
-			block, err := aes.NewCipher(key)
-			if err != nil {
-				panic(err.Error())
-			}
-
-			aesgcm, err := cipher.NewGCM(block)
-			if err != nil {
-				panic(err.Error())
-			}
-
-			plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
-			if err != nil {
-				panic(err.Error())
-			}
-
-			fmt.Printf("%s\n", plaintext)
-			// Output: exampleplaintext
-		*/
 		go sendResponse(ser, remoteaddr)
 	}
-
 }
